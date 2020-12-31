fixing errors values and adding precision and recall for detection

CenterNet/src/demo.py
@@ -28,6 +28,9 @@ true_positive = [0] * (num_cat +1)
 false_negative = [0] * (num_cat +1)
 false_positive = [0] * (num_cat +1)
 
+#same thing for the detection
+tp_fp_fn_det = [0, 0, 0]
+
 #define the lists for storing the errors 
 vol_errors = []
 iou_errors = []
@@ -71,7 +74,7 @@ def demo(opt):
       #image id for my dataset
       #image_id = image_name[image_name.rfind('/') + 1: image_name.rfind('.')]
 
-      #image id for kitti
+      #image id for KITTI
       image_id = image_name[image_name.rfind('/') + 1: image_name.rfind('.')]
       #i cut the zero from the beginning of the image name   
       while(image_id.startswith("0") and len(image_id) > 1):
@@ -85,22 +88,29 @@ def demo(opt):
       #print(res)
       det_ann = getMultipleDetAnnotation(res)
       gt_ann = getMultipleGtAnnotation(ann_file_path, image_id)
+
       if(len(gt_ann) <= 0):
         print("ground truth of the image not found")
         continue
-
+        
+      #i print the number of object detected and the number of object in the ground truth
       print("det elements = " + str(len(det_ann)))
       print("gt elements = " + str(len(gt_ann)))
 
+      '''
+      #i print the detected and the ground truth elements
       print("detection annotation list = ")
       for _ in det_ann:
         print(str(_) + "\n")
       print("groung truth annotation list = ")
       for _ in gt_ann:
         print(str(_) + "\n")
-      vol_err, loc_err, rot_err, iou = getMultipleStatistics(gt_ann, det_ann, true_positive, false_positive, false_negative)
+      '''
+      
+      #i get the statistics of the image. The mean of the statistics of each object in the image
+      vol_err, loc_err, rot_err, iou = getMultipleStatistics(gt_ann, det_ann, true_positive, false_positive, false_negative, tp_fp_fn_det)
 
-
+      ############################## OLD CODE - for detecting just one element at time
       #for single object per image
       #gt_annotation = getGtAnnotation(ann_file_path, image_id)
       #det_annotation = getDetAnnotation(res)
@@ -109,35 +119,59 @@ def demo(opt):
       #print("det_annotations = " +str(det_annotation))
             
       #vol_err, loc_err, rot_err, iou = getStatistics(gt_annotation, det_annotation, true_positive, false_positive, false_negative)
-      
+      ################################
+
+      #i create an array with all the parameters for each image
       vol_errors.append(vol_err)
-      #loc_errors.append(loc_err)
-      #rot_errors.append(rot_err)
-      #iou_errors.append(iou)
+      loc_errors.append(loc_err)
+      rot_errors.append(rot_err)
+      iou_errors.append(iou)
       
-      print("Volume error = " + str(vol_err))
-      print("Location error = " + str(loc_err))
-      print("Rotation error = "+ str(rot_err))
-      print("IoU = "+ str(iou))
-      print("TP = " + str(true_positive))
-      print("FP = " + str(false_positive))
-      print("FN = " + str(false_negative))
+      #print("Volume error = " + str(vol_err))
+      #print("Location error = " + str(loc_err))
+      #print("Rotation error = "+ str(rot_err))
+      #print("IoU = "+ str(iou))
+     
 
       time_str = ''
       for stat in time_stats:
         time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
       print(time_str)
-  
+
+  #TODO
+  #calculate the mean or some others stats of the array cumulatives of all the error value
   vol_errors_np = (np.array(vol_errors) - min(vol_errors))/float(max(vol_errors) - min(vol_errors))
   total_vol_error = np.sum(vol_errors_np) / vol_errors_np.size
+
+  rot_errors_np = (np.array(rot_errors) - min(rot_errors))/float(max(rot_errors) - min(rot_errors))
+  total_rot_error = np.sum(rot_errors_np) / vol_errors_np.size
+
+  loc_errors_np = (np.array(loc_errors) - min(loc_errors))/float(max(loc_errors) - min(loc_errors))
+  total_loc_error = np.sum(loc_errors_np) / loc_errors_np.size
+
+  total_iou = np.sum(np.array(iou_errors)) / len(iou_errors)
+
+  print(vol_errors_np.size)
+  print(rot_errors_np.size)
+  print(loc_errors_np.size)
+  print(len(iou_errors))
+
   print("Total Volume error = " + str(total_vol_error))
-  #print("Location error = " + str(loc_errors))
-  #print("Rotation error = " + str(rot_errors))
-  #print("IoU = " + str(iou_errors))
+  print("Location error = " + str(total_loc_error))
+  print("Rotation error = " + str(total_rot_error))
+  print("IoU = " + str(total_iou))
+
+  print("TP = " + str(true_positive))
+  print("FP = " + str(false_positive))
+  print("FN = " + str(false_negative))
+
+  print("TP - FP - FN detection = " + str(tp_fp_fn_det))
+
 
   for i in range(1, num_cat + 1):
     print("Precision cat " + str(i) + " = " + str(getPrecisionPerCat(i, true_positive, false_positive)))
-  
+  for i in range(1, num_cat + 1):
+    print("Recall cat " + str(i) + " = " + str(getRecallPerCat(i, true_positive, false_negative)))
 
   #get overall precision and recall
   precision = 0
@@ -146,11 +180,18 @@ def demo(opt):
     precision = precision + getPrecisionPerCat(i, true_positive, false_positive)
     recall = recall + getRecallPerCat(i, true_positive, false_negative)
   
-  #precision = precision/float(num_cat)
-  #recall = recall/float(num_cat)
+  precision = precision/float(num_cat)
+  recall = recall/float(num_cat)
 
-  #print("precision = " + str(precision))
-  #print("recall = " + str(recall))
+  print("precision classification = " + str(precision))
+  print("recall classification= " + str(recall))
+
+  precision_detection = tp_fp_fn_det[0]/float(tp_fp_fn_det[0] + tp_fp_fn_det[1])
+  recall_detection = tp_fp_fn_det[0]/float(tp_fp_fn_det[0] + tp_fp_fn_det[2])
+
+  print("precision detection = " + str(precision_detection))
+  print("recall detection = " + str(recall_detection))
+
 
     
 

CenterNet/src/my_demo_utils.py
@@ -62,8 +62,11 @@ def getMultipleGtAnnotation(filePath, image_id):
                 gt_ann_list.append(gt_annotations)
     return gt_ann_list
 
-def getMultipleStatistics(gt_ann_list, det_ann_list, true_positive, false_positive, false_negative):
+def getMultipleStatistics(gt_ann_list, det_ann_list, true_positive, false_positive, false_negative, tp_fp_fn_det):
     
+    if(len(gt_ann_list) > len(det_ann_list)):
+        tp_fp_fn_det[2] = tp_fp_fn_det[2] + (len(gt_ann_list) - len(det_ann_list))
+
     #for each element of the gt list i compare it with the closest in the det list, minimizing the distance of the centers
     right_det_element = det_ann_list[0]
     index_right_element = 0
@@ -82,10 +85,10 @@ def getMultipleStatistics(gt_ann_list, det_ann_list, true_positive, false_positi
                 index_right_element = det_ann_list.index(det_element)
         #print(index_right_element)
         #det_ann_list.remove(right_det_element)
-        print("i remove element = " + str(right_det_element) + "of index " + str(index_right_element))
+        #print("i remove element = " + str(right_det_element) + "of index " + str(index_right_element))
         det_ann_list.pop(index_right_element)
         num_detection = num_detection + 1
-        vol_err, location_err, rot_err, iou = getStatistics(gt_element, right_det_element, true_positive, false_positive, false_negative)
+        vol_err, location_err, rot_err, iou = getStatistics(gt_element, right_det_element, true_positive, false_positive, false_negative, tp_fp_fn_det)
         tot_vol_err, tot_loc_err, tot_rot_err, tot_iou =  tot_vol_err + vol_err, tot_loc_err + location_err, tot_rot_err + rot_err, tot_iou + iou
     
     #if det ann list is  not empty means that is detected something that is not in the gt, and this means a 
@@ -158,7 +161,7 @@ def getGtAnnotation(filePath, image_id):
 
 #calculation of the IoU of the front face of the bbox 3d
 #the bbox2d is defined as [topLeft.x, topLeft.y, topRight.x - topLeft.x, bottomRight.y - topLeft.y]
-def getIoU(det_bbox, gt_bbox):
+def getIoU(det_bbox, gt_bbox, tp_fp_fn_det):
   #top left point of the intersaction square
   tl_int_x = max(det_bbox[0], gt_bbox[0])
   tl_int_y = max(det_bbox[1], gt_bbox[1])
@@ -174,26 +177,18 @@ def getIoU(det_bbox, gt_bbox):
   union_area = (det_bbox[2] * det_bbox[3]) + (gt_bbox[2] * gt_bbox[3]) - int_area
 
   iou = int_area / float(union_area)
-  if iou < 0:
-      print("ATTENZIONEEEEEEEEEEE")
-      print(det_bbox)
-      print(gt_bbox)
-      print("INT AREA = " +str(int_area))
-      print("UNION AREA = " +str(union_area))
-      print("IOU = " +str(iou))
-      print("tl int = [" +str(tl_int_x) + ", " + str(tl_int_y) + "]")
-      print("tr int = [" +str(tr_int_x) + ", " + str(tl_int_y) + "]")
-      print("bl int = [" +str(tl_int_x) + ", " + str(bl_int_y) + "]")
 
-
-
+  if iou > 0.5:
+      tp_fp_fn_det[0] = tp_fp_fn_det[0] + 1
+  else:
+      tp_fp_fn_det[1] = tp_fp_fn_det[1] + 1
 
   return iou
 
 
 #calculate statistics between ground truth and detection
 #annotation order = [cat_id, dim, bbox, alpha, location, rotation_y]
-def getStatistics(gt_annotation, det_annotation, true_positive, false_positive, false_negative):
+def getStatistics(gt_annotation, det_annotation, true_positive, false_positive, false_negative, tp_fp_fn_det):
   gt_cat_id = gt_annotation[0]
   gt_dim = gt_annotation[1]
   gt_bbox = gt_annotation[2]
@@ -219,7 +214,7 @@ def getStatistics(gt_annotation, det_annotation, true_positive, false_positive, 
   vol_err = abs(det_vol - gt_vol)
 
   #check IoU of the front face of the 3dbbox
-  iou = getIoU(det_bbox, gt_bbox)
+  iou = getIoU(det_bbox, gt_bbox, tp_fp_fn_det)
 
   #check the location of the center of the 3dbbox error using the euclidean distance
   location_err = np.linalg.norm(np.array(gt_location) - np.array(det_location))